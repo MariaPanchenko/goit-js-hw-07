e516c5f40bcf2111258be6571837c98f66229dd7
MariaPanchenko <marysa2502@gmail.com>
2023-06-10 20:42:00 +0300

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+for (const item of galleryItems) {
+  const img = document.createElement('img');
+  img.src = item.preview;
+  img.alt = item.description;
+  img.className = 'gallery__image';
+
+  const a = document.createElement('a');
+  a.className = 'gallery__link';
+  a.href = item.original;
+  a.appendChild(img);
+
+  const li = document.createElement('li');
+  li.className = 'gallery__item';
+  li.appendChild(a);
+
+  galleryEl.appendChild(li);
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
